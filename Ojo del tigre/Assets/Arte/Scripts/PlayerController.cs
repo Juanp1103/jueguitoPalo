c1392688902f68d7aa28e61f3c650b158c3e2647
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-05-19 17:57:24 -0500

    Inicio de animaciones de camilo

--- Ojo del tigre/Assets/Arte/Scripts/PlayerController.cs
+++ Ojo del tigre/Assets/Arte/Scripts/PlayerController.cs
@@ -14,10 +14,14 @@ public class PlayerController : MonoBehaviour
     public int direccionDeMovimiento;
     Rigidbody2D rb;
     float cooldDownDeBoost;
+    Animator animator;
+    SpriteRenderer sr;
 
     public GameObject piedra;
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         boost = 1;
         puedeMoverse = true; puedeSaltar = true;
         rb = GetComponent<Rigidbody2D>();
@@ -30,6 +34,23 @@ public class PlayerController : MonoBehaviour
         Saltar();
         TirarPiedra();
 
+        if(direccionDeMovimiento == -1)
+        {
+            sr.flipX = true;
+        }
+        else { sr.flipX=false;}
+
+        if (!tocandoSuelo)
+        {
+            animator.SetBool("Caminando", false);
+            animator.SetBool("Saltando", true);
+        }
+        else
+        {
+            animator.SetBool("Saltando", false);
+        }
+
+
         if(cooldDownDeBoost > 0)
         {
             cooldDownDeBoost -= Time.deltaTime;
@@ -38,6 +59,8 @@ public class PlayerController : MonoBehaviour
         {
             boost = 1;
         }
+
+        
     }
 
     public void Moverse()
@@ -49,18 +72,31 @@ public class PlayerController : MonoBehaviour
                 if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
                 {
                     direccionDeMovimiento = 0;
+                    animator.SetBool("Caminando", false);
                 }
                 else if (!Input.GetKey(KeyCode.A))
                 {
+                    if (tocandoSuelo)
+                    {
+                        animator.SetBool("Caminando", true);
+                    }
                     direccionDeMovimiento = 1;
                 }
-                else { direccionDeMovimiento = -1; }
+                else 
+                { 
+                    direccionDeMovimiento = -1; 
+                    animator.SetBool("Caminando", true); 
+                }
 
                 Vector2 movimiento = new Vector2
                     (transform.position.x + (velocidadDeMovimiento * direccionDeMovimiento * boost * Time.deltaTime),
                     transform.position.y);
 
                 transform.position = movimiento;
+            }
+            else
+            {
+                animator.SetBool("Caminando", false);
             }
         }
     }
@@ -73,7 +109,10 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.W))
             {
                 rb.velocity = new Vector2(rb.velocity.x, fuerzaDeSalto);
+                
             }
+            
+            
         }
 
     }
@@ -92,7 +131,12 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            animator.SetBool("Lanzando",true);
             GameObject.Instantiate(piedra, transform.position,Quaternion.identity);
+        }
+        else
+        {
+            animator.SetBool("Lanzando", false);
         }
     }
 
