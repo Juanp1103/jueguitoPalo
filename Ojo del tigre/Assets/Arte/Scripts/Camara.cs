d7e4f7f6478ad8cffd578e28e02bf02d8c8784a8
Juanp1103 <juanp.hernandezv@upb.edu.co>
2024-05-19 14:55:19 -0500

    movimiento camara y plataformas

---
+++ Ojo del tigre/Assets/Arte/Scripts/Camara.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject target;
+
+    private float targetPoseX;
+    private float targetPoseY;
+
+    private float poseX;
+    private float poseY;
+
+    public float izquierdaMax;
+    public float derechaMax;
+
+    public float alturaMax;
+    public float alturaMin;
+
+    public float speed;
+    public bool encendida = true;
+
+    void Awake()
+    {
+        poseX = targetPoseX + derechaMax;
+        poseY = targetPoseY + alturaMin;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(poseX, poseY, -1), 1);
+    }
+
+    private void MoveCam()
+    {
+        
+        if (encendida)
+        {
+            if (target)
+            {
+                targetPoseX = target.transform.position.x;
+                targetPoseY = target.transform.position.y;
+
+                if (targetPoseX > derechaMax && targetPoseX < izquierdaMax)
+                {
+                    poseX = targetPoseX;
+                }
+
+                if (targetPoseY < alturaMax && targetPoseY > alturaMin)
+                {
+                    poseY = targetPoseY;
+                }
+            }
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(poseX, poseY, -1), speed * Time.deltaTime);
+        }
+    }
+
+    void Update()
+    {
+        MoveCam();
+    }
+}
